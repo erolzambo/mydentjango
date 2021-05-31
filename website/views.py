fc54a5b1df572fe7c91693230321e005aa703bb3
Erol Bilican <erol.bilican@gmail.com>
2021-05-31 22:45:03 +0800

    first commit

---
+++ website/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+
+def contact(request):
+    if request.method == 'POST':
+        # do some staff
+
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        # send email
+        send_mail(
+            message_name,  # subject
+            message,  # message
+            message_email,  # from email
+            ['mangenclik@gmail.com'],  # to email
+            fail_silently=False,
+        )
+        return render(request, 'contact.html', {'message_name': message_name})
+    else:
+        # return to page
+        return render(request, 'contact.html', {})
