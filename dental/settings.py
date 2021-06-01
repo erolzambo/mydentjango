f31aae06885d039e89250cc90c45cd353407ba78
Erol Bilican <erol.bilican@gmail.com>
2021-06-01 21:16:43 +0800

    tweaked app for heroku

--- dental/settings.py
+++ dental/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 import django_heroku
 import dj_database_url
 from decouple import config
